7eb1b6d7b63e97b1873e45e0689779670efa5a5c
StéphaneDucasse <stephane.ducasse@inria.fr>
2019-11-09 17:33:50 +0100

    stupidly lost the previous ones :(
    Yes this is a duplication but it works

---
+++ _support/latex/ensure-deps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+xargs tlmgr install <<DEPS
+    ctablestack
+    environ
+    filehook
+    fira
+    fontspec
+    footmisc
+    gentium-tug
+    graphics
+    ifetex
+    import
+    listings
+    luaotfload
+    luatexbase
+    memoir
+    ms
+    multirow
+    opensans
+    pgf
+    polyglossia
+    tcolorbox
+    tools
+    trimspaces
+    ulem
+    varwidth
+    xcolor
+DEPS
